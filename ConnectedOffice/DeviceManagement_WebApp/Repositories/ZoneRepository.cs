2dcb0d22e9de81fe3bdb63ce123c099769d028ef
Rudolph van Rensburg <rudolph.vanrensburg@gmail.com>
2022-09-26 11:39:40 +0200

    Zone Transfer and Repository Implementation
    Transfer all data access operations from the Zones controller to the Zones Repository class &  Implement the use of the Zone repository class in the Zone controller in lace of data access operations that have been transferred to the repository class

--- ConnectedOffice/DeviceManagement_WebApp/Repositories/ZoneRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repositories/ZoneRepository.cs
@@ -1,6 +1,86 @@
-﻿namespace DeviceManagement_WebApp.Repositories
+﻿using DeviceManagement_WebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System;
+using DeviceManagement_WebApp.Models;
+using System.Collections.Generic;
+
+namespace DeviceManagement_WebApp.Repositories
 {
     public class ZoneRepository
     {
+        private readonly ConnectedOfficeContext _context;
+
+        public ZoneRepository(ConnectedOfficeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Zones
+        public async Task<List<Zone>> GetZoneListAsync()
+        {
+            return await _context.Zone.ToListAsync();
+        }
+
+        // GET: Zones/Details/5
+        public async Task<Zone> GetDetailAsync(Guid? id)
+        {
+            return await _context.Zone
+                .FirstOrDefaultAsync(m => m.ZoneId == id);
+        }
+
+        public async void CreateZone(Zone zone)
+        {
+            zone.ZoneId = Guid.NewGuid();
+            _context.Add(zone);
+            await _context.SaveChangesAsync();
+        }
+
+        // GET: Zones/Edit/5
+        public async Task<Zone> FindAsync(Guid? id)
+        {
+            return await _context.Zone.FindAsync(id);
+        }
+
+        // POST: Zones/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async void EditAsync(Guid id,Zone zone)
+        {
+            try
+            {
+                _context.Update(zone);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+        }
+
+        // GET: Zones/Delete/5
+        public async void DeleteAsync(Guid? id)
+        {
+            var zone = await _context.Zone
+                .FirstOrDefaultAsync(m => m.ZoneId == id);
+
+            _context.Remove(zone);
+            await _context.SaveChangesAsync();
+        }
+
+        // POST: Zones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async void DeleteConfirmedAsync(Guid id)
+        {
+            var zone = await _context.Zone.FindAsync(id);
+            _context.Zone.Remove(zone);
+            await _context.SaveChangesAsync();
+        }
+
     }
 }
